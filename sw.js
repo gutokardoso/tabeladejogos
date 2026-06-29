e27751a4225bc0cab014549537578025bf70983d
Guto <gutokardoso@gmail.com>
2026-06-29 18:06:51 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'copa-2026-v10-live-clock-official';
+const CACHE_NAME = 'copa-2026-v11-hero-215';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './data.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
